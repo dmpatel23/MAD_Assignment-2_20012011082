adc9ce10f186ce0f6dc84cbd773665df829402a0
dmpatel23 <pateldevmanishkumar@gmail.com>
2023-10-23 11:24:09 +0530

    commit

--- app/src/main/java/com/example/mad_assignment_2_20012011082/MainActivity.kt
+++ app/src/main/java/com/example/mad_assignment_2_20012011082/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.mad_assignment_2_20012011082
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button1 = findViewById<Button>(R.id.btn_login)
+        val username = findViewById<EditText>(R.id.txt_username)
+        val password = findViewById<EditText>(R.id.txt_password)
+
+        button1.setOnClickListener {
+            if (username.text.toString().isEmpty()) {
+                Toast.makeText(this, "Please enter username", Toast.LENGTH_SHORT).show()
+            } else if (password.text.toString().isEmpty()) {
+                Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show()
+            } else if (password.text.toString().length < 8) {
+                Toast.makeText(this, "Please enter a valid password (at least 8 characters)", Toast.LENGTH_SHORT).show()
+            } else {
+                // If conditions are met, navigate to the Second activity
+                val intent = Intent(this, Second::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/mad_assignment_2_20012011082/Second.kt
@@ -0,0 +1,52 @@
+package com.example.mad_assignment_2_20012011082
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.text.NumberFormat
+
+class Second : AppCompatActivity() {
+
+    private lateinit var kilometers: EditText
+    private lateinit var mileage: EditText
+    private lateinit var price: EditText
+    private lateinit var calculateButton: Button
+    private lateinit var fareTextView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        kilometers = findViewById(R.id.Kilometers)
+        mileage = findViewById(R.id.Mileage)
+        price = findViewById(R.id.Price)
+        calculateButton = findViewById(R.id.Calculate)
+        fareTextView = findViewById(R.id.Fare)
+
+        calculateButton.setOnClickListener { calculateFare() }
+    }
+
+    private fun calculateFare() {
+        val kms = kilometers.text.toString()
+        val distance = kms.toDouble()
+
+        val avg = mileage.text.toString()
+        val average = avg.toDouble()
+
+        val rs = price.text.toString()
+        val petrol = rs.toDouble()
+
+        val myExpense =
+            (distance.toBigDecimal().div(average.toBigDecimal())) * petrol.toBigDecimal()
+        val fare = myExpense + myExpense
+
+        val formattedFare = NumberFormat.getCurrencyInstance().format(fare)
+        fareTextView.text = getString(R.string.Total, formattedFare)
+    }
+}
+
+
+
